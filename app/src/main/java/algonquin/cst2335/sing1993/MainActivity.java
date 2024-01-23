f774ffef247a61d6940ad6cc52d534f6beebea5a
Simran1993 <harsimranjits5102@gmail.com>
2024-01-22 21:05:58 -0500

    add th Textview , myedit and on click function

--- app/src/main/java/algonquin/cst2335/sing1993/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/sing1993/MainActivity.java
@@ -3,12 +3,28 @@ package algonquin.cst2335.sing1993;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mytext = findViewById(R.id.textview);
+        EditText myedit = findViewById(R.id.myedittext);
+        Button btn = findViewById(R.id.mybutton);
+         btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+            String editString=myedit.getText().toString();
+                mytext.setText( "Your edit text has: " + editString);
+            }
+        });
     }
+
 }
